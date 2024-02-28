5a7b2cb543982284797f8d6eec3c36d16ee0da4c
tamasbeniczky <tamas.beniczky@stud.uni-corvinus.hu>
2024-02-28 17:18:41 +0100

    vegso

--- zh_gyakorlas.sql
+++ zh_gyakorlas.sql
@@ -1,16 +1,16 @@
 --feladat1
 SELECT *,
-        -- uj oszlop
-        IIF(MONTH(METTOL) IN (6,7,8), 'Igen', 'Nem') AS 'Nyári_e'
-FROM Foglalas
-WHERE ugyfel_fk = 'laszlo2' AND GYERMEK_SZAM = 0
+                -- uj oszlop
+                IIF(MONTH(METTOL) IN (6,7,8), 'Igen', 'Nem') AS 'Nyári_e'
+        FROM Foglalas
+        WHERE ugyfel_fk = 'laszlo2' AND GYERMEK_SZAM = 0
 
---feladat2
- SELECT szh.TIPUS, YEAR(f.METTOL) AS 'Év', MONTH(f.METTOL) AS 'Hónap', COUNT(*)
- FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID
-                    JOIN Szallashely szh ON sz.SZALLAS_FK = szh.SZALLAS_ID
- WHERE DATEDIFF(day, f.METTOL, f.MEDDIG) >=5
+        --feladat2
+        SELECT szh.TIPUS, YEAR(f.METTOL) AS 'Év', MONTH(f.METTOL) AS 'Hónap', COUNT(*)
+        FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID
+                        JOIN Szallashely szh ON sz.SZALLAS_FK = szh.SZALLAS_ID
+        WHERE DATEDIFF(day, f.METTOL, f.MEDDIG) >=5
 
- GROUP BY szh.TIPUS, YEAR(f.METTOL), MONTH(f.METTOL)
+        GROUP BY szh.TIPUS, YEAR(f.METTOL), MONTH(f.METTOL)
 
 --feladat3
